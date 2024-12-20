a30266fbdd6211bfcc90adb7c938317252745e36
Uralbek-UZ <uralbek.v1.0.0@gmail.com>
2024-12-20 23:25:24 +0500

    update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+// Menyu function
 function ochish() {
   document.getElementById("menu").style.display = "block";
 }
-
 function yopish() {
     document.getElementById("menu").style.display = "none";
 }
+
+// HTML DOM
+
+document.getElementById("nashr").innerHTML = "(00157)";
